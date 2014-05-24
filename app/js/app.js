90cf27236e9f2ff22725b6325aff7591ebc42e6b
Ronald Roskens <ronald.roskens@gmail.com>
2014-05-23 22:13:30 -0500

    wip: update

--- app/js/app.js
+++ app/js/app.js
@@ -3,43 +3,129 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('opennms', [
-  'ui',
-  'ui.router',
-  'opennms.filters',
-  'opennms.services',
-  'opennms.directives',
-  'opennms.controllers'
+    'ui',
+    'ui.router',
+    'ngTable',
+    'opennms.filters',
+    'opennms.services',
+    'opennms.directives',
+    'opennms.controllers'
 ])
-.config(function($stateProvider) {
-    $stateProvider.state('node.view', {
-      url: '/node/:id',
-      controller: 'NodeController',
-      resolve: {
-        node: function($stateParams, NodeService) {
-          return NodeService.get($stateParams.id);
-        },
-        currentTimeframe: function(node) {
-          return node.getCurrentTimeframe();
-        }
-      }
-    });
-})
-.config(['$urlRouterProvider', function($urlRouterProvider) {
-    $urlRouterProvider.when('/node', '/node/list');
-    $urlRouterProvider.otherwise('');
-}])
-.config(['$stateProvider', function($stateProvider) {
-  $stateProvider
-    .state('index', {url: ''})
-    .state('login', {url: '/login',templateUrl: 'partials/login.html', title: 'Login'})
-    .state('logoff', {url: '/logoff',templateUrl: 'partials/logoff.html', title: 'Logoff'})
-    .state('view1', {url: '/view1',templateUrl: 'partials/partial1.html', controller: 'MyCtrl1', title: 'View1'})
-    .state('view2', {url: '/view2',templateUrl: 'partials/partial2.html', controller: 'MyCtrl2', title: 'View2'})
-    .state('node', {url: '/node', abstract: true })
-    .state('node.list', { url: '/list', templateUrl: 'partials/node/list.html', controller: 'NodeController', title: 'Node List'})
-    .state('node.search', {url: '/search',templateUrl: 'partials/node/search.html', controller: 'NodeController', title: 'Node Search'})
-    .state('/asset', {url: '/asset',templateUrl: 'partials/asset.html', controller: 'AssetController', title: 'Asset Search'})
-    .state('/asset/nodelist', {url: '/asset/nodelist',templateUrl: 'partials/asset/nodelist.html', controller: 'AssetListController', title: 'Node Asset List'})
-    .state('/outages', {url: '/outages',templateUrl: 'partials/outages.html', controller: 'OutagesController', title: 'Outages'})
-    .state('/pathOutages', {url: '/pathOutages',templateUrl: 'partials/pathOutages.html', controller: 'PathOutagesController', title: 'Path Outages'});
-}]);
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state('home', {
+                url: '/home',
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'partials/login.html',
+                title: 'Login'
+            })
+            .state('logoff', {
+                url: '/logoff',
+                templateUrl: 'partials/logoff.html',
+                title: 'Logoff'
+            })
+            .state('view1', {
+                url: '/view1',
+                templateUrl: 'partials/partial1.html',
+                controller: 'MyCtrl1',
+                title: 'View1'
+            })
+            .state('view2', {
+                url: '/view2',
+                templateUrl: 'partials/partial2.html',
+                controller: 'MyCtrl2',
+                title: 'View2'
+            })
+            .state('node', {
+                abstract: true,
+                url: '/node',
+                template: '<ui-view/>'
+                    //templateUrl: 'partials/node.html'
+            })
+            .state('node.default', {
+                url: '',
+                templateUrl: 'partials/node/search.html',
+                // controller: 'NodeController',
+                title: 'Node List'
+            })
+            .state('node.list', {
+                url: '/list',
+                templateUrl: 'partials/node/list.html',
+                controller: 'NodeController',
+                title: 'Node List'
+            })
+            .state('node.search', {
+                url: '/search',
+                templateUrl: 'partials/node/search.html',
+                // controller: 'NodeController',
+                title: 'Node Search'
+            })
+            .state('node.node', {
+                url: '/:id',
+                templateUrl: 'partials/node/node.html',
+                controller: 'NodeDetailController',
+                title: 'Node Detail List'
+            })
+            .state('/asset', {
+                url: '/asset',
+                templateUrl: 'partials/asset.html',
+                controller: 'AssetController',
+                title: 'Asset Search'
+            })
+            .state('/asset/nodelist', {
+                url: '/asset/nodelist',
+                templateUrl: 'partials/asset/nodelist.html',
+                controller: 'AssetListController',
+                title: 'Node Asset List'
+            })
+            .state('outages', {
+                abstract: true,
+                url: '/outages',
+                template: '<ui-view/>'
+            })
+            .state('outages.default', {
+                url: '',
+                templateUrl: 'partials/outages/search.html',
+                // controller: 'OutagesController',
+                title: 'Outages'
+            })
+            .state('outages.list', {
+                url: '/list',
+                templateUrl: 'partials/outages/list.html',
+                controller: 'OutagesController',
+                title: 'Outages List'
+            })
+            .state('outages.search', {
+                url: '/search',
+                templateUrl: 'partials/outages/search.html',
+                // controller: 'NodeController',
+                title: 'Outages Search'
+            })
+            .state('outages.outage', {
+                url: '/:id',
+                templateUrl: 'partials/outages/outage.html',
+                controller: 'OutageDetailController',
+                title: 'Outages List'
+            })
+            .state('/pathOutages', {
+                url: '/pathOutages',
+                templateUrl: 'partials/pathOutages.html',
+                controller: 'PathOutagesController',
+                title: 'Path Outages'
+            });
+    })
+    .config(['$provide', function($provide) {
+            $provide.decorator('$rootScope', function($delegate) {
+                var _emit = $delegate.$emit;
+
+                $delegate.$emit = function() {
+                    console.log.apply(console, arguments);
+                    _emit.apply(this, arguments);
+                };
+
+                return $delegate;
+            });
+        }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,108 +2,317 @@
 
 /* Controllers */
 
-angular.module('opennms.controllers', [])
-  .factory('navLinkFactory', function($http) {
-    var links = [
-        {'name': 'Node List', 'url': '#/node/list', 'display': true},
-        {'name': 'Search', 'url': '#/node/search', 'display': true},
-        {'name': 'Outages', 'url': '#/outages', 'display': true},
-        {'name': 'Path Outages', 'url': '#/pathOutages', 'display': true},
-        {'name': 'Dashboard', 'url': '#/dashboard', 'display': true},
-        {'name': 'Events', 'url': '#/events', 'display': true},
-        {'name': 'Alarms', 'url': '#/alarms', 'display': true},
-        {'name': 'Notifications', 'url': '#/notifications', 'display': true},
-        {'name': 'Assets', 'url': '#/assets', 'display': true},
-        {'name': 'Reports', 'url': '#/reports', 'display': true},
-        {'name': 'Charts', 'url': '#/charts', 'display': true},
-        {'name': 'Surveillance', 'url': '#/surveillance', 'display': true},
-        {'name': 'Maps', 'url': '#/maps', 'display': true},
-        {'name': 'Add Node', 'url': '#/element/addnode', 'display': true},
-        {'name': 'Admin', 'url': '#/admin', 'display': true},
-        {'name': 'FAQs', 'url': 'http://www.opennms.org/index.php/FAQ',  'display': true}
-    ];
-    var factory = {};
-    factory.getNavLinks = function() {
-      return links;
-    };
-    return factory;
-  })
-  .controller('onmsController', ['$scope', function($scope) {
-    $scope.page = {
-        'title': 'OpenNMS',
-        'nonavbar' : false,
-        'nobreadcrumbs': false,
-        'nofaq': false
-    };
-    $scope.user = {
-        'loggedin': true,
-        'name' : 'roskens'
-    };
-    $scope.format = 'MMM d,yyyy HH:mm Z';
-  }])
-  .controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+angular.module('opennms.controllers', ['ngTable', 'ngResource'])
+    .factory('navLinkFactory', function($http) {
+        var links = [
+            {'name': 'Node List', 'url': '#/node/list', 'display': true},
+            {'name': 'Search', 'url': '#/node/search', 'display': true},
+            {'name': 'Outages', 'url': '#/outages', 'display': true},
+            {'name': 'Path Outages', 'url': '#/pathOutages', 'display': true},
+            {'name': 'Dashboard', 'url': '#/dashboard', 'display': true},
+            {'name': 'Events', 'url': '#/events', 'display': true},
+            {'name': 'Alarms', 'url': '#/alarms', 'display': true},
+            {'name': 'Notifications', 'url': '#/notifications', 'display': true},
+            {'name': 'Assets', 'url': '#/assets', 'display': true},
+            {'name': 'Reports', 'url': '#/reports', 'display': true},
+            {'name': 'Charts', 'url': '#/charts', 'display': true},
+            {'name': 'Surveillance', 'url': '#/surveillance', 'display': true},
+            {'name': 'Maps', 'url': '#/maps', 'display': true},
+            {'name': 'Add Node', 'url': '#/element/addnode', 'display': true},
+            {'name': 'Admin', 'url': '#/admin', 'display': true},
+            {'name': 'FAQs', 'url': 'http://www.opennms.org/index.php/FAQ', 'display': true}
+        ];
+        var factory = {};
+        factory.getNavLinks = function() {
+            return links;
+        };
+        return factory;
+    })
+    .factory('breadcrumbsFactory', function($http) {
+        var breadcrumbs = [
+            {'name': 'FAQs', 'url': 'http://www.opennms.org/index.php/FAQ', 'display': true},
+            {'name': 'FAQs', 'url': 'http://www.opennms.org/index.php/FAQ', 'display': true}
+        ];
+        var factory = {};
+        factory.getBreadcrumbs = function() {
+            return breadcrumbs;
+        };
+        return factory;
+    })
+    .factory('nodeFactory', function($http) {
+        var nodes = [
+            {'id': 1, 'label': 'node8', 'foreignSource': 'bigreq', 'foreignId': 'node8'},
+            {'id': 2, 'label': 'node3'},
+            {'id': 3, 'label': 'node7'},
+            {'id': 4, 'label': 'node5'},
+            {'id': 5, 'label': 'node6'},
+            {'id': 6, 'label': 'node4'},
+            {'id': 7, 'label': 'node2'},
+            {'id': 8, 'label': 'node1'},
+            {'id': 9, 'label': 'node10'},
+            {'id': 10, 'label': 'node9'},
+            {'id': 11, 'label': 'localhost'},
+            {'id': 12, 'label': 'Test Node'},
+            {'id': 13, 'label': 'roskens-fedora'}
+        ];
+        var factory = {};
+        factory.getNodes = function() {
+            return nodes;
+        };
+        return factory;
+    })
+    .factory('nodeDetailFactory', function($http) {
+        var node = {'id': 1, 'label': 'node8', 'foreignSource': 'bigreq', 'foreignId': 'node8'};
+        var factory = {};
+        factory.getNode = function(id) {
+            return node;
+        };
+        return factory;
+    })
+    .controller('onmsController', ['$scope', function($scope) {
+            $scope.page = {
+                'title': 'OpenNMS',
+                'nonavbar': false,
+                'nobreadcrumbs': false,
+                'nofaq': false
+            };
+            $scope.user = {
+                'loggedin': true,
+                'name': 'roskens'
+            };
+            $scope.format = 'MMM d,yyyy HH:mm Z';
+        }])
+    .controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
 
-  }])
-  .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+        }])
+    .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', '$http', function($scope, $http) {
+        }])
+    .controller('MyCtrl2', ['$scope', '$http', function($scope, $http) {
 
-  }])
-  .controller('NavBarController', ['$scope', 'navLinkFactory', function($scope, navLinkFactory) {
-    $scope.links = [];
-    init();
-    function init() {
-        $scope.links = navLinkFactory.getNavLinks();
-    }
-  }])
-  .controller('ElementController', ['$scope', function($scope) {
+        }])
+    .controller('NavBarController', ['$scope', 'navLinkFactory', function($scope, navLinkFactory) {
+            $scope.links = [];
+            init();
+            function init() {
+                $scope.links = navLinkFactory.getNavLinks();
+            }
+        }])
+    .controller('BreadcrumbsController', ['$scope', 'breadcrumbsFactory', function($scope, breadcrumbsFactory) {
+            $scope.breadcrumbs = [];
+            init();
+            function init() {
+                $scope.breadcrumbs = breadcrumbsFactory.getBreadcrumbs();
+            }
+        }])
+    .controller('NodeController', ['$scope', 'nodeFactory', function($scope, nodeFactory) {
+            $scope.listInterfaces = false;
+            $scope.nodes = [];
+            init();
+            function init() {
+                $scope.nodes = nodeFactory.getNodes();
+            }
+            ;
+            $scope.getNodeLink = function(node) {
+                return '#/node/' + node.id;
+            };
+        }])
+    .controller('NodeDetailController', ['$scope', '$stateParams', 'nodeDetailFactory', function($scope, $stateParams, nodeDetailFactory) {
+            $scope.node = {};
+            $scope.node.label = 'node8';
+            $scope.node.id = 1;
+            $scope.node.foreignSource = 'bigreq';
+            $scope.node.foreignId = 'node8';
+            $scope.node.statusSite = '';
+            $scope.node.links = [];
+            $scope.node.resources = [];
+            $scope.node.navEntries = [];
+            $scope.node.schedOutages = '';
+            $scope.node.asset = {
+                'description': '',
+                'comments': ''
+            };
+            $scope.node.snmp = {
+                'sysName': '',
+                'sysObjectId': '',
+                'sysLocation': '',
+                'sysContact': '',
+                'sysDescription': ''
+            };
+            init();
+            function init() {
+                $scope.node = nodeDetailFactory.getNode($stateParams.id);
+            }
+            ;
+            console.log($scope.node);
+        }])
+    .controller('OutagesController', ['$scope', '$filter', '$http', '$resource', '$timeout', 'ngTableParams', function($scope, $filter, $http, $resource, $timeout, ngTableParams) {
+            var data = [
+                {
+                    id: 0,
+                    'node': {
+                        id: 0,
+                        foreignSource: '',
+                        label: ''
+                    },
+                    ipaddr: '',
+                    serviceName: '',
+                    lostServiceTime: '',
+                    regainedServiceTime: ''
+                },
+                {
+                    id: 1,
+                    node: {
+                        id: 0,
+                        foreignSource: '',
+                        label: ''
+                    },
+                    ipaddr: '',
+                    serviceName: '',
+                    lostServiceTime: '',
+                    regainedServiceTime: ''
+                },
+                {
+                    id: 2,
+                    node: {
+                        id: 0,
+                        foreignSource: '',
+                        label: ''
+                    },
+                    ipaddr: '',
+                    serviceName: '',
+                    lostServiceTime: '',
+                    regainedServiceTime: ''
+                },
+                {
+                    id: 3,
+                    node: {
+                        id: 0,
+                        foreignSource: '',
+                        label: '',
+                    },
+                    ipaddr: '',
+                    serviceName: '',
+                    lostServiceTime: '',
+                    regainedServiceTime: ''
+                },
+                {
+                    id: 4,
+                    node: {
+                        id: 0,
+                        foreignSource: '',
+                        label: ''
+                    },
+                    ipaddr: '',
+                    serviceName: '',
+                    lostServiceTime: '',
+                    regainedServiceTime: ''
+                },
+                {
+                    id: 5,
+                    node: {
+                        id: 0,
+                        foreignSource: '',
+                        label: ''
+                    },
+                    ipaddr: '',
+                    serviceName: '',
+                    lostServiceTime: '',
+                    regainedServiceTime: ''
+                }
+            ];
+            var Api = $resource('/opennms/opennms/rest/outages');
+            $scope.getStatusColor = function(outage) {
 
-  }])
-  .controller('ElementListController', ['$scope', function($scope) {
+            };
 
-  }])
-  .controller('OutagesController', ['$scope', function($scope) {
+            $scope.getStatusLabel = function(outage) {
 
-  }])
-  .controller('PathOutagesController', ['$scope', function($scope) {
+            };
+            
+            $scope.getServiceName = function(outage) {
+                return outage.monitoredService.serviceName;
+            }
 
-  }])
-  .controller('DashboardController', ['$scope', function($scope) {
+            $scope.tableParams = new ngTableParams({
+                page: 1, // show first page
+                count: 10, // count per page
+                filter: {
+                },
+                sorting: {
+                    id: 'desc'     // initial sorting
+                }
+            }, {
+                counts: [10, 25, 50, 100, 250, 500, 1000, 2000],
+                total: data.length, // length of data
+                getData: function($defer, params) {
+                    // use build-in angular filter
+                    
+                    // limit: count
+                    // offset: (page - 1) * count
+                    // comparator:
+                    // orderBy:
+                    Api.get({limit:params.count(), offset: (params.page() - 1) * params.count(), orderBy: 'id'}, function(data) {
+                        $timeout(function() {
+                            console.log('outages:', data.outage);
+                            var filteredData = params.filter() ?
+                                $filter('filter')(data.outage, params.filter()) :
+                                data.outage;
+                            var orderedData = params.sorting() ?
+                                $filter('orderBy')(filteredData, params.orderBy()) :
+                                data.outage;
+                            params.total(orderedData.length); // set total for recalc pagination
+                            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                        }, 5000);
+                    });
+                }
+            });
 
-  }])
-  .controller('EventsController', ['$scope', function($scope) {
+        }])
+    .controller('OutageDetailController', ['$scope', '$stateParams', function($scope, $stateParams) {
 
-  }])
-  .controller('AlarmsController', ['$scope', function($scope) {
+        }])
+    .controller('OutagesSearchController', ['$scope', '$stateParams', function($scope, $stateParams) {
 
-  }])
-  .controller('NotificationsController', ['$scope', function($scope) {
+        }])
+    .controller('PathOutagesController', ['$scope', function($scope) {
 
-  }])
-  .controller('AssetController', ['$scope', function($scope) {
+        }])
+    .controller('DashboardController', ['$scope', function($scope) {
 
-  }])
-  .controller('AssetListController', ['$scope', function($scope) {
+        }])
+    .controller('EventsController', ['$scope', function($scope) {
 
-  }])
-  .controller('ReportsController', ['$scope', function($scope) {
+        }])
+    .controller('AlarmsController', ['$scope', function($scope) {
 
-  }])
-  .controller('ChartsController', ['$scope', function($scope) {
+        }])
+    .controller('NotificationsController', ['$scope', function($scope) {
 
-  }])
-  .controller('SurveillanceController', ['$scope', function($scope) {
+        }])
+    .controller('AssetController', ['$scope', function($scope) {
 
-  }])
-  .controller('MapsController', ['$scope', function($scope) {
+        }])
+    .controller('AssetListController', ['$scope', function($scope) {
 
-  }])
-  .controller('AdminController', ['$scope', function($scope) {
+        }])
+    .controller('ReportsController', ['$scope', function($scope) {
 
-  }])
-  .controller('SupportController', ['$scope', function($scope) {
+        }])
+    .controller('ChartsController', ['$scope', function($scope) {
 
-  }]);
+        }])
+    .controller('SurveillanceController', ['$scope', function($scope) {
+
+        }])
+    .controller('MapsController', ['$scope', function($scope) {
+
+        }])
+    .controller('AdminController', ['$scope', function($scope) {
+
+        }])
+    .controller('SupportController', ['$scope', function($scope) {
+
+        }]);
 
 // NavBarController
